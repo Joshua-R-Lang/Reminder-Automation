3ab94bff84c83a45d2b8b034952abdd515d06a88
Joshua Lang <joshuarlangg@gmail.com>
2024-09-04 22:19:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main():
             print(reminder_row.to_string())
         else:
             print("No reminder found or an error occurred.")
+    
+        create = str(input("Would you like to create a reminder yes or no: "))
+        if create == "yes":
+            reminder_name = str(input("What is the reminder name: "))
+            reminder_description = str(input("What is the reminder description: "))
+            event_start = str(input("What is the event start time (format - HH:MM:SS): "))
+            event_end = str(input("What is the event end time (format - HH:MM:SS): "))
+            create_reminder_row(engine, reminder_name, reminder_description, event_start, event_end)
         
         delete = str(input("Would you like to delete a reminder yes or no: "))
         if delete == "yes":
